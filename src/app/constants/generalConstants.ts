13237d524fea6292169cedf35f56403676b894b9
George Cherian <georgcher@gmail.com>
2021-04-26 11:15:34 -0400

    Code mod for mac temp fix

--- src/app/constants/generalConstants.ts
+++ src/app/constants/generalConstants.ts
@@ -1,4 +1,4 @@
 export class GeneralConstants {
-    public static defaultLat:number = 40;
-    public static defaultLong:number = -73;
+    public static defaultLat:number = 40.803945717659126;
+    public static defaultLong:number = -73.96291668411706;
 }
